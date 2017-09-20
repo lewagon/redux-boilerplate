2233a8c3f297166e34b07748d2f1dfc949c30599
Edward Schults <edward.schults@gmail.com>
2017-09-20 11:09:46 +0200

    added redux store Provider in index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 // external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 // internal modules
 import '../assets/stylesheets/application.scss';
@@ -10,4 +13,9 @@ import '../assets/stylesheets/application.scss';
 const Hello = props => <h1>Hello, {props.name}</h1>;
 
 // render an instance of the component in the DOM
-ReactDOM.render(<Hello name="Boris" />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <Hello name="Boris" />
+  </Provider>,
+  document.querySelector('.container')
+);
